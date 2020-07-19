89862ddd9dd733f2e8a9a3c12834a72b5abd07ca
wagonhelm <jkf@ualberta.ca>
2020-07-19 15:52:24 -0600

    started game

---
+++ Sailboat/Assets/Scripts/headSailScript.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class headSailScript : MonoBehaviour
+{
+    private float maxJibSheet = 90;
+    private float minJibSheet = 0;
+
+    bool jibSheetIn;
+    bool jibSheetOut;
+
+    public float jibSheet = 0;
+    public Text jibSailStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jibSailStatus.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Get Inputs */
+        jibSheetIn = Input.GetKey(KeyCode.O);
+        jibSheetOut = Input.GetKey(KeyCode.P);
+
+        /* Adjust Sheets */
+        if (jibSheetOut && jibSheet < maxJibSheet)
+        {
+            jibSheet += 1;
+            transform.RotateAround(transform.parent.position, transform.parent.up, 1f);
+        }
+        if (jibSheetIn && jibSheet > minJibSheet)
+        {
+            jibSheet -= 1;
+            transform.RotateAround(transform.parent.position, transform.parent.up, -1f);
+        }
+
+        jibSailStatus.text = "Headsail: " + jibSheet.ToString();
+    }
+}
---
+++ Sailboat/Assets/Scripts/mainSailScript.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mainSailScript : MonoBehaviour
+{
+
+
+    private float maxMainSheet = 90;
+    private float minMainSheet = 0;
+
+    bool mainSheetIn;
+    bool mainSheetOut;
+
+    public float mainSheet = 0;
+    public Text mainSailStatus;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainSailStatus.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Get Inputs */
+
+        mainSheetIn = Input.GetKey(KeyCode.K);
+        mainSheetOut = Input.GetKey(KeyCode.L);
+
+        /* Adjust Sheets */
+        if (mainSheetOut && mainSheet < maxMainSheet) 
+        {
+            mainSheet += 1;
+            transform.RotateAround(transform.parent.position, transform.parent.up, 1f);
+        }
+        if (mainSheetIn && mainSheet > minMainSheet)
+        {
+            mainSheet -= 1;
+            transform.RotateAround(transform.parent.position, transform.parent.up, -1f);
+        }
+
+            mainSailStatus.text = "Mainsail: " + mainSheet.ToString();
+        
+    }
+}
